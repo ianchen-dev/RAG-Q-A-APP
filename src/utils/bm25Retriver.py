e45419e6c281d8dd1ae65b71eb761f12f5a4e626
ian-chen <hbchen7@126.com>
2026-04-13 17:52:03 +0800

    chore: 调整

--- src/utils/bm25Retriver.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.retrievers import BM25Retriever
-
-# 创建稀疏检索器，擅长根据关键词查找相关文档
-bm25_retriever = BM25Retriever.from_documents(split_docs)
-bm25_retriever.k = 5
--- src/utils/text_splitter.py
+++
@@ -1,9 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-def get_text_splitter(chunk_size:int=500, chunk_overlap:int=50, length_function=len, add_start_index=True ):
-  text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=chunk_size, 
-    chunk_overlap=chunk_overlap,
-    length_function=length_function,
-    add_start_index=add_start_index
-    )
-  return text_splitter
